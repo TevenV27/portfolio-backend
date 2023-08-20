288510c1ccac74525572fcf1d1847008b7b4d06b
Steven Victoria <102372427+TevenV27@users.noreply.github.com>
2023-08-20 12:10:56 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import projectRoutes from './src/routes/projectRoutes.js';
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 
